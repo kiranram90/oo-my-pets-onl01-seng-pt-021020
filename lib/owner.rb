8ab4996dedff055343b2eb454ca5642f5295b2c9
kiran ram <kram@lions.molloy.edu>
2020-03-22 02:20:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,70 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+  
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats 
+    cats = []
+    
+    Cat.all.each do |cat| 
+     if cat.owner.name == self.name
+       cats << cat
+     end
+   end
+   cats
+  end
+  
+  def dogs
+    dogs = []
+    
+    Dog.all.each do |dog| 
+     if dog.owner.name == self.name
+       dogs << dog
+     end
+    end
+    dogs
+  end
+  
+  def walk_dogs
+    self.dogs.each do |dog| 
+      dog.mood = "happy"
+    end
+  end
+  
+  def feed_cats
+    self.cats.each do |cat| 
+      cat.mood = "happy"
+    end
+  end
+  
+  def buy_cat(cat_name)
+    cat = Cat.all.find { |cat| cat.name == cat_name }
+    puts "KIRANS #{cat.name}"
+    puts "INCOMING #{cat_name}"
+    self.cats << cat
+    cat.owner = self
+  end
 end
